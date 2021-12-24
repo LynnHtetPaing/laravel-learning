5618c2ea77f6d8a72ab7505fbb9c60acfad26039
LynnHtetPaing <lin.htetpaing@seattleconsultingmyanmar.com>
2021-12-24 18:09:17 +0630

    Section-3 commit

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,18 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/app.css">
-    <link rel="stylesheet" href="/vendor-lib.css">
     <title>My Blog</title>
 </head>
 <body>
-  <article>
-      <h1>
-        {{ $post->title }}
-      </h1>
-      <p>
-        {!! $post->body !!}
-      </p>
-  </article>
-  <a href="/">Go Back</a>
+    {{ $slot }}
 </body>
 </html>
