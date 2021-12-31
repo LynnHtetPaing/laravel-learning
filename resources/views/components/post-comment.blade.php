a99a14e8ec3ff891cebbad0dbed6215325e73abf
LynnHtetPaing <lin.htetpaing@seattleconsultingmyanmar.com>
2021-12-31 17:23:26 +0630

    section_10 episode_52

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,16 @@
+<article class="flex bg-gray-100 p-6 border border-gray-200 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="http://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded">
+    </div>
+    <div>
+        <header class="mb-4>
+            <h3 class="font-bold">Lin Htet</h3>
+            <p class="text-xs">Posted 
+                <time>8 months ago</time>
+            </p>
+        </header>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, totam! Ipsam asperiores nisi dignissimos debitis eaque obcaecati animi quam, odio aliquam rem id! Fugiat, magnam porro. Excepturi quae maxime voluptatibus!
+        </p>
+    </div>
+</article>
