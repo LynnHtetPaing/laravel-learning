b409a60d8769eb5b5b5ff734c20fb384cd46c53b
LynnHtetPaing <lin.htetpaing@seattleconsultingmyanmar.com>
2021-12-31 14:09:38 +0630

    section_9 episode 45,46

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use App\Models\Post;
 use App\Models\User;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +20,7 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', [PostController::class,'index'])->name('home');
-
 Route::get('posts/{post:slug}', [PostController::class,'show']);
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
