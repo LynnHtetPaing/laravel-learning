8be01865c2bdb380c1af28672e69719b56cf7d90
LynnHtetPaing <lin.htetpaing@seattleconsultingmyanmar.com>
2022-01-03 02:00:56 +0630

    section_12 episode_68,69

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use App\Services\NewsLetter;
 use MailchimpMarketing\ApiClient;
+use Illuminate\Support\Facades\Gate;
 use App\Services\MailchimpNewsletter;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
@@ -35,5 +38,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Model::unguard();
+
+        Gate::define('admin', function (User $user) 
+        {
+            return $user->username === 'Lin Htet';
+        });
+
+        Blade::if('admin', function ()
+        {
+            return request()->user()?->can('admin');
+        });
     }
 }
