9213b8e0f501b757b96ad03c6ad82edda0d5c12e
LynnHtetPaing <lin.htetpaing@seattleconsultingmyanmar.com>
2022-01-02 23:43:53 +0630

    section_12 episode_66

--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -2,10 +2,11 @@
 <x-form.fields>
     <x-form.label name="{{ $name }}"/>
 
-    <textarea class="border border-gray-400 p-2 w-full"
+    <textarea class="border border-gray-400 p-2 w-full rounded"
             name="{{ $name }}"
             id="{{ $name }}"
             required
+            {{ $attributes }}
     >{{ old($name) }}</textarea>
 
     <x-form.error name="{{ $name }}"/>
--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,63 +1,18 @@
 <x-layout>
     <section class="px-6 py-8">
-        <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 p-6 rounded-xl">
-            <h1 class="text-center font-bold text-xl">Log In!</h1>
-            <form action="/login" method="POST" class="mt-10">
-                @csrf
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                            for="email"
-                    >
-                        Email
-                    </label>
+        <main class="max-w-lg mx-auto mt-10">
+            <x-panel>
+                <h1 class="text-center font-bold text-xl">Log In!</h1>
 
-                    <input class="border border-gray-400 p-2 w-full"
-                            type="email"
-                            name="email"
-                            id="email"
-                            value="{{ old('email') }}"
-                            required
-                    >
-                    @error('email')
-                        <p class="text-red-500 text-xs">{{ $message }}</p>
-                    @enderror
-                </div>
+                <form method="POST" action="/login" class="mt-10">
+                    @csrf
 
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="password"
-                    >
-                        password
-                    </label>
+                    <x-form.input name="email" type="email" autocomplete="username"/>
+                    <x-form.input name="password" type="password" autocomplete="current-password"/>
 
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="password"
-                           name="password"
-                           id="password"
-                           required
-                    >
-                    @error('password')
-                        <p class="text-red-500 text-xs">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <button type="submit"
-                            class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500"
-                    >
-                        Log In
-                    </button>
-                </div>
-                <!-- another ways of extracting errors -->
-                <!-- @if ($errors->any())
-                
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li class="text-red-500 text-xs">{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                @endif -->
-            </form>
+                    <x-form.button>Log In</x-form.button>
+                </form>
+            </x-panel>
         </main>
     </section>
 </x-layout>
